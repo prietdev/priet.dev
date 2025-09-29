cbcacbd89ca4a1d52758c58e5f41e706d2203d70
prietdev <priet.dev@gmail.com>
2025-09-29 18:30:47 -0300

    fix the bug and add funtions the page

---
+++ src/const/defaultDescription.ts
@@ -0,0 +1,5 @@
+export const defaultDescription: string = "Página oficial del desarrollador junior prietdev";
+
+export const errorNoFound: string = "Error 404"
+
+export const messageErrorDescription: string = "Error, not found the description."
